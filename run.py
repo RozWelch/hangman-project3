2b6c9925b0a1e39190f1f5d1b87919daeefe7b0c
RozWelch <rozwelch@gmail.com>
2022-09-16 12:55:09 +0000

    Add function to add _ for each letter in the word

--- run.py
+++ run.py
@@ -3,16 +3,25 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 # List of words to select randomly from
-list_of_words = ["imagination", "tea", "client", "cookie", "mode", "setting", "injury", "university", "night", "appearance", "refrigerator", "freedom", "definition", "category", "desk", "awareness", "perception", "buyer", "user""]
+list_of_words = ["imagination", "tea", "client", "cookie", "mode", "setting", "injury", "university", "night", "appearance", "refrigerator", "freedom", "definition", "category", "desk", "awareness", "perception", "buyer", "user"]
 import random
 random_word = random.choice(list_of_words)
+
+# Testing code
+print(f"for testing the code the solution is {random_word}")
+
+# Add a _ for each letter in the random word
+empty_guess = []
+for space in random_word:
+    empty_guess += "_"
+print(empty_guess)
 
 # Get guess from user
 letter_guess = input("Guess a letter: ").lower()
 
 # Check if letter guessed is a letter in the word
 for letter in random_word:
-    if letter == guess:
+    if letter == letter_guess:
         print("Correct")
     else:
         print("Incorrect")
